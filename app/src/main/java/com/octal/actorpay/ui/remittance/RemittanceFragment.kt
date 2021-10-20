384be89566f892b31c1e422499fe4d759a95f553
sagarKumawatOctal <sagar.kumawat@octalinfosolution.com>
2021-10-20 11:00:28 +0530

    "Remittance UI"

---
+++ app/src/main/java/com/octal/actorpay/ui/remittance/RemittanceFragment.kt
@@ -0,0 +1,22 @@
+package com.octal.actorpay.ui.remittance
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.octal.actorpay.R
+
+class RemittanceFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_remittance, container, false)
+    }
+}
