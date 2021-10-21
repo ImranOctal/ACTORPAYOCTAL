3c1fa3eea9abd1954eef0a54ad11acaa9acb86e6
Sohans-1997 <sohan.singh@octalinfosolution.com>
2021-10-21 17:15:59 +0530

    new design

---
+++ app/src/main/java/com/octal/actorpay/ui/pay_via_nfc/PayViaNfcFragment.kt
@@ -0,0 +1,42 @@
+package com.octal.actorpay.ui.pay_via_nfc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.octal.actorpay.databinding.FragmentRaiseConcernsBinding
+import com.octal.actorpay.viewmodel.ActorPayViewModel
+import org.koin.android.ext.android.inject
+
+
+class PayViaNfcFragment : Fragment() {
+    private val viewModel: ActorPayViewModel by  inject()
+    private var _binding: FragmentRaiseConcernsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentRaiseConcernsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        init()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun init() {
+
+    }
+}
